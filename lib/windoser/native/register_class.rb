a87d0461946c6f4d436ff2bc4aa91bb276a4d767
myun2ext <myun2@mbm.nifty.com>
2014-05-02 13:35:09 +0900

    Update register_class.rb
    native_call method implement

--- lib/windoser/native/register_class.rb
+++ lib/windoser/native/register_class.rb
@@ -3,6 +3,11 @@ module Windoser
     module RegisterClass
       def register
       end
+
+      private
+      def native_call(wndclsex)
+        Win32API.new('user32', 'RegisterClassEx', %w(p), 'i').call(wndclsex)
+      end
     end
   end
 end
